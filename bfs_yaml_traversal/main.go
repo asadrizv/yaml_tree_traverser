42ba0b9ee07bb544c812fa2319a8481e8400b022
asad rizvii <asadimranrizvi@hotmail.com>
2021-03-17 17:23:04 +0500

    feat(bfs_yaml_traversal): Added new mapping/data storage stategies for node data with line info

--- bfs_yaml_traversal/main.go
+++ bfs_yaml_traversal/main.go
@@ -7,14 +7,25 @@ import (
 	"log"
 )
 
-type LineVal struct {
+type NodeLineInfo struct {
 	lineNum  int
 	NodeData interface{}
 }
 
 func main() {
-	var LineNumMap []LineVal
-	yamlFile, err := ioutil.ReadFile("deployment.yaml")
+	LineNumData := ParseLineDataToStruct("deployment.yaml")
+	LineNumDataMap := ParseLineDataToMap("deployment.yaml")
+	LineParsedMap := ParseLineDataString("deployment.yaml")
+
+	fmt.Println("Standard parsing of lineNum data: ", LineNumData)
+	fmt.Println("Dictionary based parsing of lineNum data: ", LineNumDataMap)
+	fmt.Println("Consolidated line-wise parsing of lineNum data: ", LineParsedMap)
+
+}
+
+func ParseLineDataString(fileName string) map[int]string {
+	LineNumMap := make(map[int]string)
+	yamlFile, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		log.Printf("yamlFile.Get err   #%v ", err)
 	}
@@ -33,14 +44,17 @@ func main() {
 
 		if len(currentNode.Value) != 0 {
 			lineNum := currentNode.Line
-			fmt.Println("Node Value: ", currentNode.Value, ", Node line: ", currentNode.Line)
-			var processedNode LineVal
-			processedNode.lineNum = lineNum
-			err = currentNode.Decode(&processedNode.NodeData)
-			if err != nil{
+			var nodeData string
+			err = currentNode.Decode(&nodeData)
+			if err != nil {
 				fmt.Println("error in decoding node: ", err)
 			}
-			LineNumMap = append(LineNumMap, processedNode)
+			val, ok := LineNumMap[lineNum]
+			if ok{
+				LineNumMap[lineNum] = val + ": " +nodeData
+			} else{
+				LineNumMap[lineNum]+=nodeData
+			}
 
 		}
 
@@ -49,5 +63,82 @@ func main() {
 		}
 
 	}
-	fmt.Println("Line Num Map", LineNumMap)
+	return LineNumMap
+}
+
+func ParseLineDataToMap(fileName string) map[int][]NodeLineInfo {
+	LineNumMap := make(map[int][]NodeLineInfo)
+	yamlFile, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+	var nodeQue []yaml.Node
+	var root yaml.Node
+
+	err = yaml.Unmarshal(yamlFile, &root)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	nodeQue = append(nodeQue, *root.Content[0])
+	for len(nodeQue) > 0 {
+		var currentNode yaml.Node
+		currentNode, nodeQue = nodeQue[0], nodeQue[1:]
+
+		if len(currentNode.Value) != 0 {
+			lineNum := currentNode.Line
+			var processedNode NodeLineInfo
+			processedNode.lineNum = lineNum
+			err = currentNode.Decode(&processedNode.NodeData)
+			if err != nil {
+				fmt.Println("error in decoding node: ", err)
+			}
+
+			LineNumMap[lineNum] = append(LineNumMap[lineNum], processedNode)
+		}
+
+		for _, child := range currentNode.Content {
+			nodeQue = append(nodeQue, *child)
+		}
+
+	}
+	return LineNumMap
+}
+
+func ParseLineDataToStruct(fileName string) []NodeLineInfo {
+	var LineNumMap []NodeLineInfo
+	yamlFile, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+	var nodeQue []yaml.Node
+	var root yaml.Node
+
+	err = yaml.Unmarshal(yamlFile, &root)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	nodeQue = append(nodeQue, *root.Content[0])
+	for len(nodeQue) > 0 {
+		var currentNode yaml.Node
+		currentNode, nodeQue = nodeQue[0], nodeQue[1:]
+
+		if len(currentNode.Value) != 0 {
+			lineNum := currentNode.Line
+			var processedNode NodeLineInfo
+			processedNode.lineNum = lineNum
+			err = currentNode.Decode(&processedNode.NodeData)
+			if err != nil {
+				fmt.Println("error in decoding node: ", err)
+			}
+			LineNumMap = append(LineNumMap, processedNode)
+		}
+
+		for _, child := range currentNode.Content {
+			nodeQue = append(nodeQue, *child)
+		}
+
+	}
+	return LineNumMap
 }
